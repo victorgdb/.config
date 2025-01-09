15f3f29d8b90e7663dcfacce20c16ee1070dd356
Victor <victor@carbonfact.com>
2025-01-09 13:51:55 +0100

    revert

--- nvim/lua/plugins/dadbod.lua
+++ nvim/lua/plugins/dadbod.lua
@@ -1,21 +1,19 @@
 return {
-  {
-    'victorgdb/vim-dadbod-ui',
-    dependencies = {
-      { 'tpope/vim-dadbod',                lazy = true },
-      { 'victorgdb/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql', 'dbout' }, lazy = true },
-    },
-    -- cmd = {
-    --   'DBUI',
-    --   'DBUIToggle',
-    --   'DBUIAddConnection',
-    --   'DBUIFindBuffer',
-    --   'DBCompletionClearCache',
-    -- },
-    init = function()
-      -- Your DBUI configuration
-      vim.g.db_ui_use_nerd_fonts = 1
-      vim.g.db_adapter_bigquery_dataset = 'kaya'
-    end,
+  'victorgdb/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'victorgdb/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql', 'dbout' }, lazy = true },
   },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+    'DBCompletionClearCache',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_adapter_bigquery_dataset = 'kaya'
+  end,
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -170,10 +170,7 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { 'lsp', 'path', 'dadbod' },
-        providers = {
-          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
-        },
+        default = { 'lsp', 'path', 'buffer' },
       },
     },
     opts_extend = { "sources.default" }
